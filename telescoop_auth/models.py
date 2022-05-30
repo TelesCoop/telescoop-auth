5696b586eefd05f5169e8c6914376426e6d78e4e
Maxime Bellec <maxbellec@gmail.com>
2022-05-30 15:11:35 +0200

    [~] better admin and password working

--- telescoop_auth/models.py
+++ telescoop_auth/models.py
@@ -7,6 +7,9 @@ from .managers import UserManager
 
 
 class User(AbstractBaseUser):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
     username = None
     email = models.EmailField(
         verbose_name="email address",
@@ -15,7 +18,7 @@ class User(AbstractBaseUser):
     )
     first_name = models.CharField(max_length=150, default="")
     last_name = models.CharField(max_length=150, default="")
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     activation_key = models.UUIDField(default=uuid.uuid4, editable=False)
